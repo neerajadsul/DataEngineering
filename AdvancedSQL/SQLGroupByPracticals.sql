dff66daf7f1dd1b139d4f9827128c149c18db914
neerajadsul <neeraj.adsul@gmail.com>
2023-10-11 19:28:27 +0100

    Grouping and Joining tables

--- AdvancedSQL/SQLGroupByPracticals.sql
+++ AdvancedSQL/SQLGroupByPracticals.sql
@@ -27,3 +27,71 @@ ORDER BY
 	dow_total_amount
 LIMIT 3;
 
+
+-- @ Return the total sales per day, along with the number of movies rented for that day
+SELECT
+	SUM(amount) AS day_amount,
+	DATE_TRUNC('day', payment_date) as day_of_payment,
+	COUNT(rental_id) as rentals_on_day
+FROM
+	payment
+GROUP BY
+	day_of_payment
+ORDER BY
+	day_of_payment;
+
+
+-- @block ind the id's of all customers who have spent over $100 
+-- during the course of their membership
+SELECT
+	customer_id,
+	SUM(amount) AS total
+FROM
+	payment
+GROUP BY
+	customer_id
+HAVING
+	SUM(amount) > 100.0
+ORDER BY
+	total;
+
+
+-- @block Find the average amount spent per film rating. 
+-- Return the film rating and the amount.
+SELECT
+	film.rating,
+	ROUND(AVG(payment.amount), 2) AS avg_amount_rating
+FROM
+	payment
+INNER JOIN
+	rental ON rental.rental_id = payment.rental_id
+INNER JOIN
+	inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN
+	film ON film.film_id = inventory.film_id
+GROUP BY
+	film.rating;
+
+
+-- @block How many rented films have yet to be returned?
+SELECT
+	(COUNT(rental.rental_id) - COUNT(return_date)) AS films_to_return
+FROM
+	rental;
+
+
+-- @block How many copies of the film 'HUNCHBACK IMPOSSIBLE' exist in the inventory system? 
+SELECT
+	film.film_id,
+	film.title,
+	COUNT(inventory.film_id)
+FROM
+	film
+INNER JOIN
+	inventory ON inventory.film_id = film.film_id
+GROUP BY
+	film.film_id
+HAVING
+	film.title = 'HUNCHBACK IMPOSSIBLE'
+
+
