875d4a80b70506ba680f7adb762e5ec6631a36f3
neerajadsul <neeraj.adsul@gmail.com>
2023-10-17 22:52:43 +0100

    Skeleton class structure for data cleaning steps.

--- DataCentralisationProject/data_cleaning.py
+++ DataCentralisationProject/data_cleaning.py
@@ -1,5 +1,10 @@
 """Clean data from each of the data sources."""
+from pandas import DataFrame
 
 
 class DataCleaning:
-    pass
+    @staticmethod
+    def clean_user_data(user_df: DataFrame, dest_table: str):
+        """Clean the user data for NULL values, errors with dates,
+        incorrectly typed values and rows filled with the wrong information."""
+        print(user_df.head())
