ef809c24935cc78ec06488dcb63e3d9b5e97b825
neerajadsul <neeraj.adsul@gmail.com>
2023-10-17 22:48:11 +0100

    Skeleton class for data extractor methods.

--- DataCentralisationProject/data_extractor.py
+++ DataCentralisationProject/data_extractor.py
@@ -1,5 +1,22 @@
 """Extract data from CSV files, a web API or S3 bucket."""
+import pandas as pd
+from pandas import DataFrame
+import tabula
+
+from database_utils import DatabaseConnector
 
 
 class DataExtractor:
+    @staticmethod
+    def read_rds_table(engine, src_table: str) -> DataFrame:
+        """Read database table into Pandas DataFrame."""
+        pass
+
+    @staticmethod
+    def retrieve_pdf_data():
+        """Read and extract text data from pdf file from AWS S3 bucket."""
+        data_file = r'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+        pass
+
+if __name__ == "__main__":
     pass
