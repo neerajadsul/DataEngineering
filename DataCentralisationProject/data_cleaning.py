9f24b692a3aedd21c9c673d4d0b4fc257388f0d9
neerajadsul <neeraj.adsul@gmail.com>
2023-10-19 18:31:55 +0100

    Refactor emails cleaning into separate method.

--- DataCentralisationProject/data_cleaning.py
+++ DataCentralisationProject/data_cleaning.py
@@ -15,24 +15,32 @@ class DataCleaning:
         incorrectly typed values and rows filled with the wrong information."""
         # Check for duplicates
         users_df.drop_duplicates(inplace=True)
+
         # Cleaning date columns: date_of_birth and join_date
         users_df['date_of_birth'] = pd.to_datetime(users_df['date_of_birth'], infer_datetime_format=True, errors='coerce')
         users_df['join_date'] = pd.to_datetime(users_df['join_date'], infer_datetime_format=True, errors='coerce')
+
         # Check invalid date when after current date
         users_df.loc[users_df['date_of_birth'] > np.datetime64(datetime.datetime.now()), 'date_of_birth'] = np.NaN
         users_df.loc[users_df['join_date'] > np.datetime64(datetime.datetime.now()), 'join_date'] = np.NaN
+
         # Check for invalid email address: email_address
-        mask_invalid = self.email_validation(users_df['email_address'])
-        users_df.loc[mask_invalid, 'email_address'] = ""
-        # Check for valid phone numbers: phone_number
-        # Keep the international format
+        users_df['email_address'] = self.email_validation(users_df['email_address'])
 
         # log some statistics for investigation when needed
         logger.debug('Number of NaNs: ', users_df.isna().sum())
         logger.debug('Number of Nulls: ', users_df.isnull().sum())
 
     def email_validation(self, emails: Series) -> Series:
+        """
+        Replace invalid emails with empty string.
+        :param emails: Series
+        :return: cleaned-up Series
+        """
         # Ref: https://uibakery.io/regex-library/email-regex-python
-        email_regex = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+        email_regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
         mask_invalid = ~emails.str.match(email_regex)
-        return mask_invalid
+        emails.loc[mask_invalid] = str()
+        return email
+
+
