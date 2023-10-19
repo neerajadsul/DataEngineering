a8802083d79a5e416cc18cf06824af8fbd85f033
neerajadsul <neeraj.adsul@gmail.com>
2023-10-19 11:45:53 +0100

    Clean date columns for invalid and missing values.

--- DataCentralisationProject/data_cleaning.py
+++ DataCentralisationProject/data_cleaning.py
@@ -1,10 +1,31 @@
 """Clean data from each of the data sources."""
+import logging
 from pandas import DataFrame
+import pandas as pd
+import numpy as np
+import datetime
+
+logger = logging.getLogger('data_cleaning')
 
 
 class DataCleaning:
     @staticmethod
-    def clean_user_data(user_df: DataFrame, dest_table: str):
+    def clean_user_data(users_df: DataFrame, dest_table: str):
         """Clean the user data for NULL values, errors with dates,
         incorrectly typed values and rows filled with the wrong information."""
-        print(user_df.head())
+        # Check for duplicates
+        users_df.drop_duplicates(inplace=True)
+        # Cleaning date columns: date_of_birth and join_date
+        users_df['date_of_birth'] = pd.to_datetime(users_df['date_of_birth'], infer_datetime_format=True, errors='coerce')
+        users_df['join_date'] = pd.to_datetime(users_df['join_date'], infer_datetime_format=True, errors='coerce')
+        # Check invalid date when after current date
+        users_df.loc[users_df['date_of_birth'] > np.datetime64(datetime.datetime.now()), 'date_of_birth'] = np.NaN
+        users_df.loc[users_df['join_date'] > np.datetime64(datetime.datetime.now()), 'join_date'] = np.NaN
+        # Check for invalid email address: email_address
+
+        # Check for valid phone numbers: phone_number
+        # Keep the international format
+
+        # log some statistics for investigation when needed
+        logger.debug('Number of NaNs: ', users_df.isna().sum())
+        logger.debug('Number of Nulls: ', users_df.isnull().sum())
