067145ecc986cc494d135c7c38c213c6f711a74f
neerajadsul <neeraj.adsul@gmail.com>
2023-11-14 19:09:05 +0000

    Encapsulate inner working, reduce duplication.

--- DataCentralisationProject/database_utils.py
+++ DataCentralisationProject/database_utils.py
@@ -6,9 +6,9 @@ from pandas import DataFrame
 
 
 class DatabaseConnector:
-    @staticmethod
-    def read_db_creds(creds_file='db_creds.yaml'):
-        creds = None
+    """Methods to interact with a database with given credentials."""
+
+    def __init__(self, creds_file):
         try:
             with open(creds_file) as f:
                 creds = yaml.load(f, yaml.Loader)
@@ -16,35 +16,27 @@ class DatabaseConnector:
             print(f'File not found, {creds_file}')
         except IOError:
             print(f'Found but could not access {creds_file}')
-        return creds
+        self.engine = self.__init_db_engine(creds=creds)
 
     @staticmethod
-    def init_db_engine(creds) -> Engine:
-        db_type = 'postgresql'
-        db_api = 'psycopg2'
+    def __init_db_engine(creds, db_type='postgresql', db_api='psycopg2') -> Engine:
         db_host = creds['RDS_HOST']
         db_user = creds['RDS_USER']
         db_pass = creds['RDS_PASSWORD']
         db_name = creds['RDS_DATABASE']
         db_port = creds['RDS_PORT']
-        engine = create_engine(
-            f'{db_type}+{db_api}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-        )
+        engine = create_engine(f'{db_type}+{db_api}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
         return engine
 
-    @staticmethod
-    def list_db_tables(engine: Engine):
-        inspector = inspect(engine)
+    def list_db_tables(self) -> list:
+        inspector = inspect(self.engine)
         table_names = inspector.get_table_names()
         return table_names
 
-    @staticmethod
-    def upload_to_db(users_df: DataFrame, dest_table: str):
+    def upload_to_db(self, df: DataFrame, dest_table: str) -> None:
         """Store Pandas DataFrame to database with given table name."""
-        creds = DatabaseConnector.read_db_creds(creds_file='db_creds_central.yaml')
-        engine = DatabaseConnector.init_db_engine(creds)
-        users_df.to_sql(dest_table, engine, if_exists='replace', index=False)
+        df.to_sql(dest_table, self.engine, if_exists='replace', index=False)
 
 
 if __name__ == '__main__':
-    pass
+    dbc = DatabaseConnector(creds_file='db_creds_central.yaml')
