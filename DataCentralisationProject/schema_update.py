6560f4e85a77f968253a9a125809ecaf4e28aa39
neerajadsul <neeraj.adsul@gmail.com>
2023-11-20 16:19:01 +0000

    Refactor for reusable methods for any table.

--- DataCentralisationProject/schema_update.py
+++ DataCentralisationProject/schema_update.py
@@ -22,42 +22,52 @@ class DbSchemaUpdate:
         | product_quantity | BIGINT             | SMALLINT           |
         +------------------+--------------------+--------------------+
         """
+        TABLE_NAME = 'dim_orders'
         # Convert date and user columns to UUIDs
-        query_date_user = text('''ALTER TABLE dim_orders
-                                  ALTER COLUMN date_uuid TYPE uuid USING date_uuid::uuid,
-                                  ALTER COLUMN user_uuid TYPE uuid USING user_uuid::uuid;''')
-        with self.db_connector.engine.connect() as conn:
-            conn.execute(query_date_user)
-            conn.commit()
-
+        self._convert_to_uuid(TABLE_NAME, 'date_uuid')
         # Convert product quantity to SMALLINT from BIGINT
-        query_prod_quantity = text('''ALTER TABLE dim_orders
-                                   ALTER COLUMN product_quantity TYPE smallint;''')
-        with self.db_connector.engine.connect() as conn:
-            conn.execute(query_prod_quantity)
-            conn.commit()
+        self._convert_to_smallint(TABLE_NAME, 'product_quantity')
+        # Convert card_number to TEXT 
+        self._convert_to_text(TABLE_NAME, 'card_number')
+        # Covert card_number to VARCHAR with maximum length for card number.
+        self._convert_to_varchar(TABLE_NAME, 'card_number', max_data_length=True)
+        # Convert store_code and product_code to VARCHAR with maximum length for the respective code
+        self._convert_to_varchar('store_code', max_data_length=True)
+        self._convert_to_varchar('product_code', max_data_length=True)
 
-        # Convert card_number to TEXT and then to VARCHAR with maximum length of the string.
-        query_card_number = text('''ALTER TABLE dim_orders ALTER COLUMN card_number TYPE TEXT;''')
-        with self.db_connector.engine.connect() as conn:
-            conn.execute(query_card_number)
-            conn.commit()
-        self._convert_to_varchar_max_length('card_number')
-
-        self._convert_to_varchar_max_length('store_code')
-        self._convert_to_varchar_max_length('product_code')
-
-    def _convert_to_varchar_max_length(self, column_name):
-        # Convert store_code to VARCHAR with maximum length of the string.
-        query_max_length = text(f'SELECT MAX(LENGTH({column_name})) FROM dim_orders')
-        with self.db_connector.engine.connect() as conn:
-            conn.execute(query_max_length)
-            max_length = conn.execute(text(f'SELECT MAX(LENGTH({column_name})) FROM dim_orders'))
-        max_length = max_length.scalar_one()
+    def _convert_to_varchar(self, table_name, column_name, max_data_length=True):
+        if isinstance(max_data_length, bool) and max_data_length:
+            query_max_length = text(f'SELECT MAX(LENGTH({column_name})) FROM dim_orders')
+            with self.db_connector.engine.connect() as conn:
+                conn.execute(query_max_length)
+                max_length = conn.execute(text(f'SELECT MAX(LENGTH({column_name})) FROM dim_orders'))
+            max_length = max_length.scalar_one()
+        elif not isinstance(max_data_length, int) or max_data_length < 1:
+            raise ValueError(f'Varchar(?) length should be integer > 0, got {max_data_length}.')
 
         query_modify = text(f'ALTER TABLE dim_orders ALTER COLUMN {column_name} TYPE VARCHAR({max_length})')
         with self.db_connector.engine.connect() as conn:
             conn.execute(query_modify)
+            conn.commit()
+
+    def _convert_to_smallint(self, table_name, column_name):
+        query_smallint = text(f'''ALTER TABLE {table_name}
+                                   ALTER COLUMN {column_name} TYPE smallint;''')
+        with self.db_connector.engine.connect() as conn:
+            conn.execute(query_smallint)
+            conn.commit()
+
+    def _convert_to_text(self, table_name, column_name):
+        query_to_text = text(f'''ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE TEXT;''')
+        with self.db_connector.engine.connect() as conn:
+            conn.execute(query_to_text)
+            conn.commit()
+
+    def _convert_to_uuid(self, table_name, column_name):
+        query_uuid = text(f'''ALTER TABLE {table_name}
+                                  ALTER COLUMN {column_name} TYPE uuid USING {column_name}::uuid;''')
+        with self.db_connector.engine.connect() as conn:
+            conn.execute(query_uuid)
             conn.commit()
 
 
