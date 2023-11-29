e3f06147e0393c0fda5b9f7eb7ab0b678cdb2ed8
neerajadsul <neeraj.adsul@gmail.com>
2023-11-29 14:54:11 +0000

    Transfer DB credentials from code to local file.

--- PinterestPipeline/user_posting_emulation.py
+++ PinterestPipeline/user_posting_emulation.py
@@ -8,7 +8,7 @@ import sqlalchemy
 from sqlalchemy import text
 from pprint import pprint
 import argparse
-
+import yaml
 
 random.seed(100)
 
@@ -20,44 +20,45 @@ def print_keys(d):
 
 
 class AWSDBConnector:
-
     def __init__(self):
+        with open('db_creds_pinterest.yaml') as f:
+            creds = yaml.safe_load(f)
+        self.HOST = creds['HOST']
+        self.USER = creds['USER']
+        self.PASSWORD = creds['PASSWORD']
+        self.DATABASE = creds['DATABASE']
+        self.PORT = creds['PORT']
 
-        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
-        self.USER = 'project_user'
-        self.PASSWORD = ':t%;yCY3Yjg'
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
-        
     def create_db_connector(self):
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        engine = sqlalchemy.create_engine(
+            f'mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4'
+        )
         return engine
 
 
 new_connector = AWSDBConnector()
 
 
-def run_infinite_post_data_loop(num_posts=-1):       
+def run_infinite_post_data_loop(num_posts=-1):
     while num_posts > 0:
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
         engine = new_connector.create_db_connector()
 
         with engine.connect() as connection:
-
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_string = text(f'SELECT * FROM pinterest_data LIMIT {random_row}, 1')
             pin_selected_row = connection.execute(pin_string)
 
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
 
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_string = text(f'SELECT * FROM geolocation_data LIMIT {random_row}, 1')
             geo_selected_row = connection.execute(geo_string)
 
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
 
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_string = text(f'SELECT * FROM user_data LIMIT {random_row}, 1')
             user_selected_row = connection.execute(user_string)
 
             for row in user_selected_row:
@@ -78,11 +79,9 @@ def run_infinite_post_data_loop(num_posts=-1):
             num_posts -= 1
 
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     parser = argparse.ArgumentParser('Retrieve given number of posts')
-    parser.add_argument('-n', default=10000, type=int,
-                        help='number of posts, default 10000')
+    parser.add_argument('-n', default=10000, type=int, help='number of posts, default 10000')
 
     args = parser.parse_args()
 
